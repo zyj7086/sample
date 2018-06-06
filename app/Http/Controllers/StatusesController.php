17832183df5ca45ba1a05f9161c151b1b3c84e75
vagrant <vagrant@homestead>
2018-06-06 13:44:35 +0000

    Add user statuses

---
+++ app/Http/Controllers/StatusesController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Models\Status;
+use Auth;
+use Illuminate\Http\Request;
+
+class StatusesController extends Controller
+{
+    //
+    public function __construct()
+    {
+    	$this->middleware('auth');
+    }
+    public function store(Request $request)
+    {
+    	$this->validate($request,[
+    		'content' =>'required|max:140'
+    	]);
+    	Auth::user()->statuses()->create([
+    		'content'=>$request['content']
+    	]);
+    	return redirect()->back();
+    }
+    public function destroy(Status $status)
+    {
+        $this->authorize('destroy',$status);
+        $status->delete();
+        session()->flash('success','微薄已被成功删除！');
+        return redirect()->back();
+    }
+}
